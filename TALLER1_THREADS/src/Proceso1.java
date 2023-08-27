5afeef2e940534dc7e16007985e4b0fc9e0ff884
Jhon Layton <jhon.laytonr@uniagustiniana.edu.co>
2023-08-27 12:38:31 -0500

    Modificacion proceso 1 hilos. Creacion informe

--- TALLER1_THREADS/src/Proceso1.java
+++ TALLER1_THREADS/src/Proceso1.java
@@ -13,7 +13,9 @@ public class Proceso1 extends Thread{
             clip = AudioSystem.getClip(); //INTERFAZ PARA REPRODUCIR AUDIO USANDO CLIP
             clip.open(audioInputStream); //ABRIR FLUJO DE AUDIO Y PREPARAR EJECUCION
 
+
             System.out.println("Reproduciendo la cancion..");
+            System.out.println("Artista: Skrillex\nCancion: Bangarang\nLanzamiento: 16 de febrero de 2012\nGenero: Dusbtep, Electro House\nSello discografico: Big Beat Records, Atlantic Records\nDuracion: 3 minutos y 35 segundos\nEnlace: https://www.youtube.com/watch?v=YJVmu6yttiw");
             clip.start();//REPRODUCIR CANCION
         }catch(Exception e){
             e.printStackTrace(); //MANEJO DE ERRORES
